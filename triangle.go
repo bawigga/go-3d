5acebf2bc05ccbc8fcdf56846480d89fe7995670
Brian Wigginton <brianwigginton@gmail.com>
2015-02-04 21:53:10 -0600

    adds some keyboard input handling and color!

--- triangle.go
+++ triangle.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-gl/gl"
 	glfw "github.com/go-gl/glfw3"
 )
 
 func errorCallback(err glfw.ErrorCode, desc string) {
 	fmt.Printf("%v: %v\n", err, desc)
+}
+
+func keyboardHandler(w *glfw.Window, key glfw.Key, scancode int, action glfw.Action, mods glfw.ModifierKey) {
+	switch key {
+	case glfw.KeyQ, glfw.KeyEscape:
+		w.SetShouldClose(true)
+	}
 }
 
 func main() {
@@ -24,13 +32,19 @@ func main() {
 		panic(err)
 	}
 
+	window.SetKeyCallback(keyboardHandler)
+
 	window.MakeContextCurrent()
 	glfw.SwapInterval(1)
 
-	gl.ClearColor(1, 0, 0)
+	gl.ClearColor(.5, 0, 0, .2)
 
 	for !window.ShouldClose() {
+
+		gl.Clear(gl.COLOR_BUFFER_BIT)
+
 		window.SwapBuffers()
+
 		glfw.PollEvents()
 	}
 }
